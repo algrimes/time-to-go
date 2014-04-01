fb729e6919c5f82a729e73ea67d2a04302ef265a
algrimes <agrimes@thoughtworks.com>
2014-04-01 18:33:13 +0100

    Al : bootstrap. made the table a bit prettier

--- src/main/java/com/testupstream/timetogo/TimeToGoApp.java
+++ src/main/java/com/testupstream/timetogo/TimeToGoApp.java
@@ -3,6 +3,8 @@ package com.testupstream.timetogo;
 import com.testupstream.timetogo.bundles.TimeToGoGuiceBundle;
 import com.testupstream.timetogo.resources.ArrivalsResource;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.servlets.assets.AssetServlet;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
@@ -17,6 +19,7 @@ public class TimeToGoApp extends Application<TimeToGoConfig> {
     public void initialize(Bootstrap<TimeToGoConfig> bootstrap) {
         bootstrap.addBundle(new ViewBundle());
         bootstrap.addBundle(new TimeToGoGuiceBundle());
+        bootstrap.addBundle(new AssetsBundle("/assets"));
     }
 
     @Override
--- src/main/resources/com/testupstream/timetogo/views/arrivals.ftl
+++ src/main/resources/com/testupstream/timetogo/views/arrivals.ftl
@@ -1,11 +1,17 @@
 <#-- @ftlvariable name="" type="com.testupstream.timetogo.views.ArrivalsView" -->
 <html>
+<head>
+    <title>Local bus departures</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
 <body>
-<table>
+<table class="table table-striped">
     <tr><th>ETA</th><th>Stop Name</th><th>Route</th></tr>
     <#list arrivals as arrival>
     <tr><td>${arrival.eta}</td><td>${arrival.stopName}</td><td>${arrival.route}</td></tr>
     </#list>
 </table>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
